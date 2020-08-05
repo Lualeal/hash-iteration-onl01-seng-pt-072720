ada1841735523626c08067f0911c748fb2ce033f
Luana Leal <luana.leal.eq@gmail.com>
2020-08-05 22:36:51 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,4 +10,14 @@ def happy_birthday(birthday_kids)
   end 
 end
 
-
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    if birthday_kids < 27 
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else 
+      puts "You are too olf for this."
+    end 
+  end 
+end 
+  
+  
